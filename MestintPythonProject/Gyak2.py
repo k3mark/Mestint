55a2be434ee652ed2f16432bc385cdb82acb7ae5
k3mark <82876635+k3mark@users.noreply.github.com>
2025-02-26 15:38:24 +0100

    Add files via upload

--- MestintPythonProject/Gyak2.py
+++ MestintPythonProject/Gyak2.py
@@ -1,5 +1,17 @@
-class kancso_feladat;
-    
+class kancso_feladat:
+    def __init__(self, ke, c):
+        self.kezdo = ke
+        self.cel = c
+        self.Max1 = 3
+        self.Max2 = 5
+        self.Max3 = 8
+
+    def celteszt(self, allapot):
+        return allapot[1] == self.cel or allapot[2] == self.cel
+
+    def rakovetkezo(self, allapot):
+        return allapot
 
 
 if __name__ == "__main__":
+    kancso_problema = kancso_feladat((0,0,8), 4)
